25b5de9de4957baaec241f8315fcdae748042556
Ridho Maulana <edoin21@gmail.com>
2023-02-21 17:45:07 +0700

    Support glob pattern

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const glob = require("glob");
+const isGlob = require("is-glob");
 const AdmZip = require("adm-zip");
 const core = require("@actions/core");
 
@@ -11,26 +13,42 @@ console.log(`Ready to zip "${files}" into ${dest}`);
 
 const zip = new AdmZip();
 
-files.split(" ").forEach(fileName => {
-  const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
+files.split(" ").forEach((fileName) => {
+  if (isGlob(fileName)) {
+    const files = glob.sync(fileName, {
+      cwd: process.env.GITHUB_WORKSPACE,
+      absolute: true,
+      nodir: !recursive,
+    });
 
-  if (!fs.existsSync(filePath)) {
-    console.log(`  - ${fileName} (Not Found)`);
-    return;
-  }
+    files.forEach((filePath) => {
+      const dir = path.dirname(filePath);
+      const zipDir = dir === process.env.GITHUB_WORKSPACE ? "" : dir;
 
-  const dir = path.dirname(fileName);
-  const stats = fs.lstatSync(filePath);
-
-  if (stats.isDirectory()) {
-    const zipDir = dir === "." ? fileName : dir;
-    zip.addLocalFolder(filePath, !recursive && zipDir);
+      zip.addLocalFile(filePath, !recursive && zipDir);
+      console.log(`  - ${filePath}`);
+    });
   } else {
-    const zipDir = dir === "." ? "" : dir;
-    zip.addLocalFile(filePath, !recursive && zipDir);
-  }
+    const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
 
-  console.log(`  - ${fileName}`);
+    if (!fs.existsSync(filePath)) {
+      console.log(`  - ${fileName} (Not Found)`);
+      return;
+    }
+
+    const dir = path.dirname(fileName);
+    const stats = fs.lstatSync(filePath);
+
+    if (stats.isDirectory()) {
+      const zipDir = dir === "." ? fileName : dir;
+      zip.addLocalFolder(filePath, !recursive && zipDir);
+    } else {
+      const zipDir = dir === "." ? "" : dir;
+      zip.addLocalFile(filePath, !recursive && zipDir);
+    }
+
+    console.log(`  - ${fileName}`);
+  }
 });
 
 const destPath = path.join(process.env.GITHUB_WORKSPACE, dest);
